b482c819a89e5af6a0cd853f553a24cab6066041
JLMaynardDesign <JLMaynardDesign@gmail.com>
2021-12-10 03:38:29 -0500

    commit

--- express_server.js
+++ express_server.js
@@ -1,15 +1,22 @@
-const { response } = require("express");
+
 const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs"); //set ejs as the view engine
-
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-//bodyParser needs to come before all routes to convert request body into readable form
+const morgan = require('morgan');
+const app = express();
+const PORT = 8080;
 
-const generateRandomString = function() {
+app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const generateRandomString = function () {
   return Math.random().toString(36).substring(2, 8);
 };
 
@@ -18,8 +25,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-//add new route handler for "/urls" and use res.rener() to pass the URL data to our template
-//routes should be orderd from most specific to least specific
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -30,8 +36,25 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]/* What goes here? */ };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.newLongURL;
+  res.redirect(`/urls/${req.params.shortURL}`);
 });
 
 app.get("/", (req, res) => {
@@ -46,29 +69,20 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  let shortURL = generateRandomString();
-  let longURL = req.body.longURL;
-  let urlDatabase = {shortURL, longURL};
-  console.log(urlDatabase);
-  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  res.redirect(`/urls/${shortURL}`);
-});
-
-//redirect short urls
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];//req.body.longURL;
-  res.redirect(longURL);
-});
-
 app.post("/urls/:shortURL/delete", (req, res) => {
-  //const shortURL = req.params.shortURL;
-  delete urlDatabase[req.params.shortURL];
+  const deleteURL = req.params.shortURL;
+  delete urlDatabase[deleteURL];
+  //console.log(res.body);
   res.redirect("/urls");
 });
 
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const newLongURL = req.body.longURL;
+  urlDatabase[id] = newLongURL;
+  res.redirect('/urls');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
