aaa4227121b2068f6e691286e4d7f53ea8e1f9f2
JLMaynardDesign <JLMaynardDesign@gmail.com>
2021-12-12 05:38:46 -0500

    commit

---
+++ helpers.js
@@ -0,0 +1,11 @@
+const findUserByEmail = (email, database) => {  
+  for (const property in database) {
+    const user = database[property];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+module.exports = findUserByEmail;
