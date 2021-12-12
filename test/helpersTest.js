27e6234765d0989c675ec0ece1627e9baca04691
JLMaynardDesign <JLMaynardDesign@gmail.com>
2021-12-12 05:47:23 -0500

    Commit now now

---
+++ test/helpersTest.js
@@ -0,0 +1,30 @@
+const { assert } = require('chai');
+
+const { findUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.equal(expectedUserID, user.id);
+  });
+
+  it('should return undefined, if an attempt to pass an email that does not already exist in the user database is committed', function() {
+    const user = findUserByEmail("user@example.com", testUsers);
+    const expectedEmail = undefined;
+    assert.equal(expectedEmail, user.email);
+  });
+});
